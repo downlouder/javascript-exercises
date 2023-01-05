f3496c508676c8bab2459ef15513530d47e824f5
downlouder <118766413+downlouder@users.noreply.github.com>
2023-01-05 02:24:56 +0200

    Completed 5, 6, 7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+    if (num1 > num2) {
+        let temp = num2;
+        num2 = num1;
+        num1 = temp;
+    }
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR";
+    }
+    
+    let result = 0;
+    for (num1; num1 <= num2; num1++) {
+        result += num1;
+    }
+    return result;
 };
 
 // Do not edit below this line

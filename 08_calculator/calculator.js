b12b9983c30ff36f89b3e4bfc1d6b33300065aa6
downlouder <118766413+downlouder@users.noreply.github.com>
2023-01-10 04:06:33 +0200

    Completed 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let result = 0;
+  if (toString.call(arr) !== "[object Array]") {
+    return 0;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    result += arr[i];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let result = 1;
+  for (let i = 0; i < arr.length; i++) {
+    result *= arr[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(x, n) {
+  let result = 1;
+  for (let i = 0; i < n; i++) {
+    result *= x;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(fac) {
+	let result = 1;
+  // let num = fac;
+  if (fac === 0) {
+    return 1;
+  }
+  for (let i = 1; i <= fac; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
